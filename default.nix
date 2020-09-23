2ca2b139628ee1f953aff83243f47d4113a72bf3
Sridhar Ratnakumar <srid@srid.ca>
2020-09-23 18:14:03 -0400

    Limit build deps to nix-shell only

--- default.nix
+++ default.nix
@@ -1,14 +1,18 @@
 let 
   pkgs = import (import ./nixpkgs.nix) { };
+  extraDeps =
+    if pkgs.lib.trivial.inNixShell
+      then with pkgs.haskellPackages; [ 
+          cabal-install
+          ghcid 
+          cabal-fmt 
+          haskell-language-server 
+          ormolu 
+        ]
+      else [];
 in 
   pkgs.haskellPackages.developPackage {
     root = ./.;
     modifier = drv:
-      pkgs.haskell.lib.addBuildTools drv (with pkgs.haskellPackages;
-        [ cabal-install
-          cabal-fmt
-          ghcid
-          haskell-language-server
-          ormolu
-        ]);
+      pkgs.haskell.lib.addBuildTools drv extraDeps;
   }
