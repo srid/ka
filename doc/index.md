2a389b7282daf2713b8aa680db8cbe70b5c93209
Sridhar Ratnakumar <srid@srid.ca>
2020-09-26 18:44:09 -0400

    ka State

---
+++ doc/index.md
@@ -0,0 +1,13 @@
+# ka User Guide
+
+`ka` lifts your plain-text notes to life.
+
+## Usage 
+
+Run `ka` on any directory containing Markdown notes.
+
+```
+ka
+```
+
+`ka` watches the current directory, applies the various plugins, and generates a self-contained website acting as a portal to your notes "lifted" to provide structured (per the plugins) view.
--- src/Main.hs
+++ src/Main.hs
@@ -2,6 +2,8 @@
 
 module Main where
 
+import qualified Commonmark as CM
+import qualified Commonmark.Pandoc as CP
 import qualified Data.Map.Strict as Map
 import Main.Utf8 (withUtf8)
 import Reflex
@@ -10,7 +12,9 @@ import Reflex.Host.Headless (MonadHeadlessApp, runHeadlessApp)
 import Reflex.Network (networkView)
 import System.FSNotify (defaultConfig)
 import qualified System.FSNotify as FSN
+import System.FilePath ((</>))
 import System.FilePattern.Directory (getDirectoryFiles)
+import qualified Text.Pandoc.Builder as B
 
 main :: IO ()
 main =
@@ -18,42 +22,79 @@ main =
     runHeadlessApp $ do
       -- Fire this to quit the app
       -- (e, fire) <- newTriggerEvent
-      kaApp
+      kaApp "./doc"
       pure never
 
-kaApp :: MonadHeadlessApp t m => m ()
-kaApp = do
+kaApp :: MonadHeadlessApp t m => FilePath -> m ()
+kaApp inputDir = do
   pb <- getPostBuild
-  let inputDir = "." <$ pb
-  changes <- watchDir defaultConfig inputDir (const True)
+  changes <- watchDir defaultConfig (inputDir <$ pb) (const True)
   fs0 :: Map FilePath Text <- fmap Map.fromList $ do
     liftIO $ do
-      files <- getDirectoryFiles "." ["*.md"]
+      files <- getDirectoryFiles inputDir ["*.md"]
       forM files $ \fp ->
-        (fp,) <$> readFileText fp
-  fsE :: Event t (Map FilePath (Maybe Text)) <-
+        (fp,) <$> readFileText (inputDir </> fp)
+  fsE :: Event t KaStateDiff <-
     performEvent $
       ffor changes $
+        -- TODO: group events
         fmap one . \case
           FSN.Added fp _ _ -> (fp,) . Just <$> readFileText fp
           FSN.Modified fp _ _ -> (fp,) . Just <$> readFileText fp
           FSN.Removed fp _ _ -> pure (fp, Nothing)
-          FSN.Unknown fp _ _ -> pure (fp, Nothing) -- ?
-  rec fsWithDiff :: Dynamic t (Map FilePath Text, Map FilePath (Maybe Text)) <-
-        holdDyn (fs0, mempty) $
-          ffor (attach (current fsWithDiff) fsE) $ \((things, _lastChanged), changed) ->
-            (patchMap things changed, changed)
-  _fs <-
-    holdUniqDyn
-      =<< listHoldWithKey fs0 fsE (\_fp s -> pure s)
+          FSN.Unknown fp _ _ -> pure (fp, Nothing) -- FIXME: ?
+  rec kaWithDiff :: Dynamic t (KaState, KaStateDiff) <-
+        holdDyn (kaInit fs0, mempty) $
+          ffor (attach (current kaWithDiff) fsE) $ \((things, _lastChanged), changed) ->
+            (kaPatch things changed, changed)
   void $
     networkView $
-      ffor fsWithDiff $ \(things, changed) -> do
-        liftIO $ print things
-        liftIO $ print changed
+      ffor kaWithDiff $ \(st, diff) -> do
+        liftIO $ print st
+        liftIO $ print diff
         liftIO $ putStrLn "===\n"
 
-patchMap :: Ord k => Map k a -> Map k (Maybe a) -> Map k a
-patchMap xs diff =
+-- ka types
+-- --------
+
+type FileContent = Text
+
+type FileDoc = Either CM.ParseError (CP.Cm () B.Blocks)
+
+data KaState = KaState
+  { -- | Pandoc AST of plain-text files
+    inputDoc :: Map FilePath FileDoc,
+    -- | Graph of notes
+    graph :: (),
+    -- | Post-transformed Pandoc AST
+    outputDoc :: Map FilePath FileDoc,
+    -- | Pages to generate
+    outputFiles :: Map FilePath ByteString
+  }
+  deriving (Show)
+
+type KaStateDiff = Map FilePath (Maybe FileContent)
+
+emptyKaState :: KaState
+emptyKaState =
+  KaState mempty () mempty mempty
+
+kaInit :: Map FilePath FileContent -> KaState
+kaInit =
+  kaPatch emptyKaState . fmap Just
+
+-- Incremental update of ka state
+-- ------------------------------
+
+kaPatch :: KaState -> KaStateDiff -> KaState
+kaPatch st diff =
   let (toAdd, toDel) = Map.mapEither (maybeToLeft ()) diff
-   in Map.union toAdd xs `Map.difference` toDel
+   in st
+        { inputDoc =
+            Map.union (flip Map.mapWithKey toAdd parseMarkdown) (inputDoc st)
+              `Map.difference` toDel
+        }
+
+parseMarkdown :: FilePath -> Text -> FileDoc
+parseMarkdown fp s =
+  join $ CM.commonmarkWith mempty fp s
