877db9a1a642b32ddcaf1f4207d50613a8153cea
Sridhar Ratnakumar <srid@srid.ca>
2020-09-28 19:48:37 -0400

    Monitor only *.md files

--- src/Ka/App.hs
+++ src/Ka/App.hs
@@ -12,34 +12,55 @@ import Reflex.FSNotify (watchDir)
 import Reflex.Host.Headless (MonadHeadlessApp)
 import System.FSNotify (defaultConfig)
 import qualified System.FSNotify as FSN
-import System.FilePath ((</>))
-import System.FilePattern.Directory (getDirectoryFiles)
+import System.FilePath (takeExtension)
+import System.FilePattern.Directory (FilePattern, getDirectoryFiles)
 
-kaApp :: MonadHeadlessApp t m => FilePath -> m (Dynamic t (Db, Map FilePath (Changed Text)))
-kaApp inputDir = do
+kaApp :: MonadHeadlessApp t m => m (Dynamic t (Db, Map FilePath (Changed Text)))
+kaApp = do
   pb <- getPostBuild
-  fileChanges <- watchDir defaultConfig (inputDir <$ pb) (const True)
+  fileChanges <- watchDir defaultConfig ("." <$ pb) (const True)
   let ctx = Ctx [demoPlugin]
   db0 :: Db <-
     Db.initDb ctx <$> do
       liftIO $ do
-        files <- getDirectoryFiles inputDir ["*.md"]
+        files <- getDirectoryFiles "." [notePattern]
         fmap Map.fromList $
           forM files $ \fp -> do
-            s <- readFileText (inputDir </> fp)
+            s <- readFileText fp
             pure (fp, s)
   dbChanges <-
-    performEvent $
-      ffor fileChanges $
+    fmap (fmapMaybe id) $
+      performEvent $
         -- TODO: group events
-        -- TODO: filter by *.md
-        fmap one . \case
-          FSN.Added fp _ _ -> (fp,) . Added <$> readFileText fp
-          FSN.Modified fp _ _ -> (fp,) . Modified <$> readFileText fp
-          FSN.Removed fp _ _ -> pure (fp, Removed)
-          FSN.Unknown fp _ _ -> pure (fp, Removed) -- FIXME: ?
+        ffor fileChanges $ \x -> liftIO $ do
+          noteChange x >>= \case
+            Nothing -> pure Nothing
+            Just (fp, change) ->
+              pure $ Just $ one (fp, change)
   rec dbWithChanges <-
         holdDyn (db0, mempty) $
           ffor (attach (current dbWithChanges) dbChanges) $ \((things, _), changes) ->
             (Db.changeDb ctx things changes, changes)
   pure dbWithChanges
+
+noteExtension :: String
+noteExtension = ".md"
+
+notePattern :: FilePattern
+notePattern = "*" <> noteExtension
+
+noteChange :: FSN.Event -> IO (Maybe (FilePath, Changed Text))
+noteChange evt =
+  if takeExtension fp == noteExtension
+    then fmap Just $ case evt of
+      FSN.Added _ _ _ ->
+        (fp,) . Added <$> readFileText fp
+      FSN.Modified _ _ _ ->
+        (fp,) . Modified <$> readFileText fp
+      FSN.Removed _ _ _ ->
+        pure (fp, Removed)
+      FSN.Unknown _ _ _ ->
+        pure (fp, Removed) -- FIXME: ?
+    else pure Nothing
+  where
+    fp = FSN.eventPath evt
--- src/Main.hs
+++ src/Main.hs
@@ -7,21 +7,23 @@ import Main.Utf8 (withUtf8)
 import Reflex (Reflex (never), ffor)
 import Reflex.Host.Headless (runHeadlessApp)
 import Reflex.Network (networkView)
+import System.Directory (withCurrentDirectory)
 
 main :: IO ()
 main =
   withUtf8 $ do
-    runHeadlessApp $ do
-      -- Fire this to quit the app
-      -- (e, fire) <- newTriggerEvent
-      dbWithDiff <- kaApp "./doc"
-      void $
-        networkView $
-          ffor dbWithDiff $ \(db, diff) -> liftIO $ do
-            -- liftIO $ print st
-            putStrLn $ "Diff: " <> show diff
-            forM_ (Map.toList $ Db.outputFiles db) $ \(k, v) -> do
-              putStr $ k <> " : "
-              print v
-            putStrLn "===\n"
-      pure never
+    withCurrentDirectory "./doc" $ do
+      runHeadlessApp $ do
+        -- Fire this to quit the app
+        -- (e, fire) <- newTriggerEvent
+        dbWithDiff <- kaApp
+        void $
+          networkView $
+            ffor dbWithDiff $ \(db, diff) -> liftIO $ do
+              -- liftIO $ print st
+              putStrLn $ "Diff: " <> show diff
+              forM_ (Map.toList $ Db.outputFiles db) $ \(k, v) -> do
+                putStr $ k <> " : "
+                print v
+              putStrLn "===\n"
+        pure never
