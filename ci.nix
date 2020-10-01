38eb1b293e46b539c4db4729f1740afb86bce1ec
Sridhar Ratnakumar <srid@srid.ca>
2020-10-01 19:55:19 -0400

    Add ci.nix

---
+++ ci.nix
@@ -0,0 +1,12 @@
+{ system ? builtins.currentSystem }:
+let
+  sources = import ./nix/sources.nix;
+  pkgs = import sources.nixpkgs { inherit system; };
+in
+pkgs.recurseIntoAttrs {
+  # Build both default.nix and shell.nix such that both derivations are
+  # pushed to cachix. This allows the development workflow (bin/run, etc.) to
+  # use cachix to full extent.
+  ka = import ./default.nix {};
+  kaStatic = import ./static.nix {};
+}
