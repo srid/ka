b626973120739a889764aedccfd9461a9b63bf4e
Sridhar Ratnakumar <srid@srid.ca>
2020-10-01 19:47:23 -0400

    avoid redundant file reads

--- src/Ka/App.hs
+++ src/Ka/App.hs
@@ -3,6 +3,7 @@
 module Ka.App where
 
 import Control.Monad.Fix (MonadFix)
+import Data.List (nubBy)
 import qualified Data.Map.Strict as Map
 import Data.Time (NominalDiffTime)
 import Ka.Database (Ctx (Ctx), Db)
@@ -30,6 +31,10 @@ ctx =
       viewGraphPlugin
     ]
 
+-- | Like `watchDir` but batches file events
+--
+-- Returned event is in the form of list, which is guaranteed to not have repeat
+-- events (i.e., 2 or more events with the same eventPath)
 watchDirWithDebounce ::
   ( PostBuild t m,
     TriggerEvent t m,
@@ -45,7 +50,14 @@ watchDirWithDebounce ms dirPath = do
   let cfg = defaultConfig {FSN.confDebounce = FSN.Debounce ms}
   pb <- getPostBuild
   evt <- watchDir cfg (dirPath <$ pb) (const True)
-  fmap toList <$> batchOccurrences ms evt
+  evtGrouped <- fmap toList <$> batchOccurrences ms evt
+  -- Discard all but the last event for each path.
+  pure $ nubByKeepLast ((==) `on` FSN.eventPath) <$> evtGrouped
+
+-- | Like @Data.List.nubBy@ but keeps the last occurence
+nubByKeepLast :: (a -> a -> Bool) -> [a] -> [a]
+nubByKeepLast f =
+  reverse . nubBy f . reverse
 
 kaApp :: MonadHeadlessApp t m => m (Dynamic t (Db, Map FilePath (Changed Text)))
 kaApp = do
@@ -63,9 +75,6 @@ kaApp = do
       performEvent $
         ffor fileChanges $ \(toList -> xs) ->
           liftIO $ do
-            -- TODO: If `xs` has multiple events for the same file, this will
-            -- trigger multiple file reads (see `noteChange`) which we should
-            -- avoid ... possibly by removing duplicates in `watchDirWithDebounce`.
             catMaybes <$> traverse noteChange xs
   rec dbWithChanges <-
         holdDyn (db0, mempty) $
@@ -84,7 +93,9 @@ noteChange evt =
       FSN.Removed _ _ _ ->
         pure (fp, Removed)
       FSN.Unknown _ _ _ ->
-        pure (fp, Removed) -- FIXME: ?
+        -- Unknown is an event that fsnotify (haskell) doesn't know how to
+        -- handle. Let's consider the file to be modified, just to be safe.
+        (fp,) . Modified <$> readFileText fp
     else pure Nothing
   where
     fp = takeFileName $ FSN.eventPath evt
