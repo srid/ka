eaae0f7530a5dfcc342c0b9eb7b0ea873a07adcd
Sridhar Ratnakumar <srid@srid.ca>
2020-10-11 14:42:35 -0400

    remove old code

--- src/Main.hs
+++ src/Main.hs
@@ -8,9 +8,7 @@ import Ka.App (kaApp)
 import Main.Utf8 (withUtf8)
 import Reflex
 import Reflex.Dom
-import Reflex.Host.Headless (runHeadlessApp)
-import System.Directory (createDirectoryIfMissing, removeFile, withCurrentDirectory)
-import System.FilePath ((</>))
+import System.Directory (removeFile, withCurrentDirectory)
 import System.IO.Error (isDoesNotExistError)
 
 notesDir :: FilePath
@@ -18,8 +16,11 @@ notesDir = "/home/srid/Sync/zk"
 
 main :: IO ()
 main =
-  withCurrentDirectory notesDir $ do
-    mainWidget bodyWidget
+  withUtf8 $
+    withCurrentDirectory notesDir $ do
+      mainWidgetWithHead
+        (el "title" $ text "ka Jsaddle")
+        bodyWidget
 
 bodyWidget ::
   ( DomBuilder t m,
@@ -39,29 +40,6 @@ bodyWidget = do
     ffor (Map.keys <$> output) $ \fs ->
       forM_ fs $ \fp -> do
         el "li" $ text $ T.pack fp
-
-oldMain :: IO ()
-oldMain =
-  withUtf8 $ do
-    let outputDir = notesDir </> ".ka" </> "output"
-    createDirectoryIfMissing True outputDir
-    withCurrentDirectory notesDir $ do
-      runHeadlessApp $ do
-        output <- kaApp
-        void $
-          performEvent_ $
-            ffor output $ \outputFiles -> liftIO $ do
-              -- putStrLn $ "Diff: " <> show diff
-              forM_ (Map.toList outputFiles) $ \(k, mGenS) -> do
-                case mGenS of
-                  Nothing -> do
-                    putStrLn $ "- " <> k
-                    removeIfExists $ outputDir </> k
-                  Just genS -> do
-                    putStrLn $ "W " <> k
-                    s <- genS
-                    writeFileBS (outputDir </> k) $! s
-        pure never
 
 removeIfExists :: FilePath -> IO ()
 removeIfExists fileName = removeFile fileName `catch` handleExists
