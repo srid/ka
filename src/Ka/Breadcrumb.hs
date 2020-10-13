6c07fe411ca4a8831b1fa5604b11c30dc4450842
Sridhar Ratnakumar <srid@srid.ca>
2020-10-12 20:04:35 -0400

    breadcrumb: capture invariants

--- src/Ka/Breadcrumb.hs
+++ src/Ka/Breadcrumb.hs
@@ -1,33 +1,33 @@
 module Ka.Breadcrumb where
 
-import Data.List (elemIndex)
-import qualified Data.List.NonEmpty as NE
 import Ka.Route
 import Reflex.Dom.Core hiding (current)
 
--- TODO: Capture these invariants in a proper datastructure
+-- | A non empty list with cursor
 data Breadcrumbs a = Breadcrumbs
-  { -- | This should be a non empty set, as there can't be duplicates
-    _breadcrumbs_trail :: NonEmpty a,
-    -- | This should always exist in the above trail.
-    _breadcrumbs_current :: a
+  { _breadcrumbs_before :: [a],
+    _breadcrumbs_current :: a,
+    _breadcrumbs_after :: [a]
   }
   deriving (Eq, Show)
 
 init :: a -> Breadcrumbs a
 init x =
-  Breadcrumbs (one x) x
+  Breadcrumbs [] x []
 
+allCrumbs :: Breadcrumbs a -> [a]
+allCrumbs (Breadcrumbs before current after) =
+  before <> [current] <> after
+
+-- | Put a new crumb, such that it behaves like the stacked navigation of
+-- https://notes.andymatuschak.org/
 putCrumb :: Eq a => a -> Breadcrumbs a -> Breadcrumbs a
-putCrumb x (Breadcrumbs trail current) =
-  if x `elem` toList trail
-    then Breadcrumbs trail x
-    else case elemIndex current (toList trail) of
-      Nothing ->
-        error "breadcrumbs error: not possible"
-      Just idx ->
-        let trail' = x :| (current : reverse (take idx $ toList trail))
-         in Breadcrumbs (NE.reverse trail') x
+putCrumb x bc@(Breadcrumbs before current _) =
+  case break (== x) (allCrumbs bc) of
+    (_, []) ->
+      Breadcrumbs (before <> [current]) x []
+    (before', _x : after') ->
+      Breadcrumbs before' x after'
 
 render ::
   (DomBuilder t m, PostBuild t m, MonadHold t m, Prerender js t m) =>
@@ -36,9 +36,10 @@ render ::
 render routeHist = do
   divClass "ui mini steps" $ do
     switchHold never <=< dyn $
-      ffor routeHist $ \Breadcrumbs {..} -> fmap (leftmost . toList) $
-        forM _breadcrumbs_trail $ \rPrev -> do
-          elClass "a" (bool "completed step" "active step" $ rPrev == _breadcrumbs_current) $ do
-            divClass "content" $ do
-              divClass "title" $ do
-                routeLink rPrev $ renderRouteText rPrev
+      ffor routeHist $ \bc@Breadcrumbs {..} -> do
+        fmap (leftmost . toList) $
+          forM (allCrumbs bc) $ \rPrev -> do
+            elClass "a" (bool "completed step" "active step" $ rPrev == _breadcrumbs_current) $ do
+              divClass "content" $ do
+                divClass "title" $ do
+                  routeLink rPrev $ renderRouteText rPrev
