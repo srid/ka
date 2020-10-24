d6d500d2659e4c2b7e122cdeace8a99448ac6c41
Sridhar Ratnakumar <srid@srid.ca>
2020-10-24 15:01:55 -0400

    Ignore notes without unchecked tasks

--- src/Ka/Plugin/Task.hs
+++ src/Ka/Plugin/Task.hs
@@ -25,7 +25,10 @@ import qualified Text.Pandoc.Walk as W
 -- - Retain tasklist hierarchy shape.
 --  - Perhaps do that in a separate TaskBoard plugin
 
-newtype Task = Task B.Block
+data Task = Task
+  { _task_block :: B.Block,
+    _task_checked :: Bool
+  }
   deriving (Eq, Show)
 
 type Tasks = Map ThingName [Task]
@@ -38,7 +41,14 @@ runPlugin ::
   (Event t (Map ThingName (Maybe Pandoc))) ->
   m (Event t (Map ThingName (Maybe Tasks)))
 runPlugin _graphD _pandocD pandocE = do
-  let tasksE = ffor pandocE $ Map.mapMaybe $ traverse (nonEmptyAsJust . extractTasks)
+  let tasksE =
+        ffor pandocE $
+          Map.mapMaybe $
+            traverse $ \(extractTasks -> ts) -> do
+              ts' <- nonEmptyAsJust ts
+              -- Ignore notes without any unchecked tasks
+              guard $ any (not . _task_checked) ts'
+              pure ts'
   tasks <- foldDyn G.patchMap mempty tasksE
   pure $
     fforMaybe (attach (current tasks) $ updated tasks) $ \(oldTasks, currTasks) -> do
@@ -56,8 +66,8 @@ extractTasks =
   W.query go
   where
     go blk = case blk of
-      B.Plain (B.Str "☐" : B.Space : _is) -> [Task blk]
-      B.Plain (B.Str "☒" : B.Space : _is) -> [Task blk]
+      B.Plain (B.Str "☐" : B.Space : _is) -> [Task blk False]
+      B.Plain (B.Str "☒" : B.Space : _is) -> [Task blk True]
       _ -> []
 
 render ::
@@ -74,5 +84,5 @@ render tasks = do
           dyn_ $ renderRouteText <$> r
       r2 <- fmap (switch . current . fmap leftmost) $
         simpleList (fmap snd xDyn) $ \taskDyn ->
-          PandocView.render $ ffor taskDyn $ \(Task blk) -> B.Pandoc mempty $ one blk
+          PandocView.render $ ffor taskDyn $ \(Task blk _checked) -> B.Pandoc mempty $ one blk
       pure $ leftmost [r1, r2]
