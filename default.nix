b0391db6399bea5ccf9412c3e4aab899cef02bed
Sridhar Ratnakumar <srid@srid.ca>
2020-11-07 14:57:22 -0500

    Advance nixpkgs

--- default.nix
+++ default.nix
@@ -23,15 +23,11 @@ in
     source-overrides = {
       reflex-fsnotify = src.reflex-fsnotify;
       reflex-dom-pandoc = src.reflex-dom-pandoc;
-      with-utf8 = src.haskell-with-utf8;
-      algebraic-graphs = src.alga;
       commonmark = src.commonmark-hs + "/commonmark";
       commonmark-extensions = src.commonmark-hs + "/commonmark-extensions";
       commonmark-pandoc = src.commonmark-hs + "/commonmark-pandoc";
     };
     overrides = self: super: with pkgs.haskell.lib; {
-      skylighting = super.skylighting_0_10_0_2;
-      skylighting-core = super.skylighting-core_0_10_0_2;
       reflex-dom = 
         if useWarp
           then addBuildDepend (enableCabalFlag super.reflex-dom "use-warp") self.jsaddle-warp
